0084bf2636fffa6ee6798f460a6135cd3c385413
justYu2001 <jack4781012@gmail.com>
2022-11-29 11:42:06 +0800

    fix: sign up page moblie layout

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -18,7 +18,7 @@ const SignUp: NextPage = () => {
                             <Logo />
                         </a>
                     </Link>
-                    <div className="text-center text-black">
+                    <div className="hidden text-center text-black">
                         <p className="md:text-2xl">北科最牛逼的購物平台</p>
                         <small className="md:text-lg">
                             Best Shopping Platform Made by NTUT CSIE
@@ -26,7 +26,7 @@ const SignUp: NextPage = () => {
                     </div>
                 </header>
 
-                <main className="mt-6 flex-1 rounded-t-[2.5rem] bg-white py-5 px-6 md:m-0 md:flex md:flex-col md:justify-center md:rounded-none md:p-0">
+                <main className="mt-3 flex-1 rounded-t-[2.5rem] bg-white py-5 px-6 md:m-0 md:flex md:flex-col md:justify-center md:rounded-none md:p-0">
                     <div className="md:px-40">
                         <Form />
                         <Link href="/signin">
@@ -67,13 +67,16 @@ function Logo() {
 function Form() {
     return (
         <>
-            <h2 className="text-center text-2xl font-medium tracking-wide md:text-3xl">
+            <h2 className="mt-2 text-center text-3xl font-medium tracking-wide">
                 註冊
             </h2>
             <form action="" className="flex flex-col pb-2">
-                <Input id="username" filedName="使用者名稱" />
-                <Input id="email" filedName="電子郵件" />
-                <Input id="password" filedName="密碼" />
+                <div className="flex flex-col">
+                    <Input id="username" filedName="使用者名稱" />
+                    <Input id="email" filedName="電子郵件" />
+                    <Input id="password" filedName="密碼" />
+                    <p className="mb-7 mt-1.5 h-4 text-lg text-red-500"></p>
+                </div>
                 <button
                     type="submit"
                     className="my-3 rounded-md bg-black py-2 text-lg text-white md:my-6"
