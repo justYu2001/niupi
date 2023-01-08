f86f3c87ef1b133c2be5de40134e285c83cf175e
justYu2001 <jack4781012@gmail.com>
2023-01-08 18:54:02 +0800

    feat: home page pc layout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,31 +7,28 @@ export default function NavBar() {
     const isLoggedIn = session.status === "authenticated";
 
     return (
-        <div className="flex justify-between p-4">
-            <h1 className="text-2xl">牛啤購物</h1>
-            <nav>
-                <ul className="flex">
-                    {isLoggedIn ? (
-                        <>
-                            <li className="mx-2">
-                                <Link href="/seller">賣家中心</Link>
-                            </li>
-                            <li className="mx-2">
-                                <button onClick={() => signOut()}>登出</button>
-                            </li>
-                        </>
-                    ) : (
-                        <>
-                            <li className="mx-2">
-                                <Link href="/signup">註冊</Link>
-                            </li>
-                            <li className="mx-2">
-                                <Link href="/signin">登入</Link>
-                            </li>
-                        </>
-                    )}
-                </ul>
-            </nav>
-        </div>
+        <nav className="float-right">
+            <ul className="flex items-center px-4 py-6 font-medium tracking-wide">
+                {isLoggedIn ? (
+                    <>
+                        <li className="mx-2">
+                            <Link href="/seller">賣家中心</Link>
+                        </li>
+                        <li className="mx-2">
+                            <button onClick={() => signOut()}>登出</button>
+                        </li>
+                    </>
+                ) : (
+                    <>
+                        <li className="mx-2">
+                            <Link href="/signin">登入</Link>
+                        </li>
+                        <li className="mx-2 rounded bg-black py-1 px-6 text-white">
+                            <Link href="/signup">註冊</Link>
+                        </li>
+                    </>
+                )}
+            </ul>
+        </nav>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
+
+import { AiOutlineSearch } from "react-icons/ai";
 
 import NavBar from "@/components/NavBar";
 
@@ -11,11 +14,41 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <header>
-                <NavBar />
-            </header>
+            <div className="h-screen bg-indian-yellow">
+                <header className="relative z-10">
+                    <NavBar />
+                </header>
+                <main className="absolute inset-0 flex items-center justify-around">
+                    <div>
+                        <h1 className="text-6xl font-medium tracking-wide">
+                            牛啤購物
+                        </h1>
+                        <label className="mt-8 flex cursor-text items-center rounded-full bg-white py-4 pl-6 pr-3 shadow-md">
+                            <input
+                                type="text"
+                                placeholder="今天想買什麼？"
+                                className="peer w-72 text-2xl outline-none"
+                            />
+                            <AiOutlineSearch className="text-3xl text-slate-400" />
+                        </label>
+                    </div>
+                    <Logo />
+                </main>
+            </div>
         </>
     );
 };
 
 export default Home;
+
+function Logo() {
+    return (
+        <div className="relative h-96 w-96">
+            <Image
+                src="/images/niupi-logo-black.png"
+                layout="fill"
+                alt="logo"
+            />
+        </div>
+    );
+}
